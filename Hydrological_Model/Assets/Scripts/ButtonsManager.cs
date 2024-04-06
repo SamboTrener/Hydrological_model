3afa3b9b99e7186896d0f71e7a8fb690f36b1329
SamboTrener <trololo1247@list.ru>
2024-04-06 17:43:06 +0300

    Base
    Map creation and erosion in uncomfortable way

---
+++ Hydrological_Model/Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsManager : MonoBehaviour
+{
+    [SerializeField] Button createTerrain;
+    [SerializeField] Button erode;
+
+    [SerializeField] int mapSize = 255;
+    [SerializeField] int erosionBrushRadius = 3;
+    [SerializeField] int numIterations = 30000;
+
+    float[] map;
+    int mapSizeWithBorder;
+
+    private void Awake()
+    {
+        var createTerrainButton = createTerrain.GetComponent<Button>();
+        createTerrainButton.onClick.AddListener(CreateTerrain);
+
+        var erodeButton = erode.GetComponent<Button>();
+        erodeButton.onClick.AddListener(Erode);
+    }
+
+    void CreateTerrain()
+    {
+        mapSizeWithBorder = mapSize + erosionBrushRadius * 2;
+        map = HeightmapGenerator.Instance.GenerateHeightMap(mapSizeWithBorder);
+        TerrainGenerator.Instance.ContructMesh(mapSize, mapSizeWithBorder, erosionBrushRadius, map);
+    }
+
+    void Erode()
+    {
+        ErosionGenerator.Instance.Erode(map, mapSizeWithBorder, numIterations);
+        TerrainGenerator.Instance.ContructMesh(mapSize, mapSizeWithBorder, erosionBrushRadius, map);
+    }
+}
